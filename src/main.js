245e11a50b4b0e08e41424cc37e8176d09e58c1c
acgotakuatdji <biao.wang@dji.com>
2020-08-28 14:18:15 +0900

    add gtag

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import VueGtag from "vue-gtag";
 
 Vue.config.productionTip = false;
+
+Vue.use(
+  VueGtag,
+  {
+    config: { id: "UA-123456-2" },
+    appName: "Hello world",
+    pageTrackerScreenviewEnabled: true
+  },
+  router
+);
 
 new Vue({
   router,
